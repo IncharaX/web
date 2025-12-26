221c61e3d55276fa837833677fd76fb917f1746d
Inchara nk <incharank1@gmail.com>
2025-12-26 19:43:36 +0530

    Conditional statements

---
+++ css/video56.html/index.js
@@ -0,0 +1,23 @@
+console.log("Hello this is conditional tutorial")
+
+let age = 1;
+let grace = 2;
+
+console.log(age+grace)
+console.log(age-grace)
+console.log(age/grace)
+console.log(age*grace)
+console.log(age**grace)
+
+if(age==18)
+{
+    console.log("You can drive");
+}
+else if(age==0){
+    console.log("Are u kidding?");
+}
+else{
+    console.log("you can't drive");
+}
+
+let c=(age>2)?('yes'):("No")
