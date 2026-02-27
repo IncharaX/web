92211f136f8707084793fa3fda62b33a6a687eff
Inchara nk <incharank1@gmail.com>
2026-02-27 16:33:55 +0530

    Commit

--- backend/video86/main.js
+++ backend/video86/main.js
@@ -1,11 +1,17 @@
-const { createServer } = require('node:http');
+// const { createServer } = require('node:http');
+// const fs = require("fs")
+
+import http from "http";
+
 const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('<h1>Hello world</h1>');
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello world!!</h1>');
 });
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
